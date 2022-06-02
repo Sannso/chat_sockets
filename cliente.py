f15d00128587353e169ebd13062b7e6e36ead8b1
Sanss <santi-2000-12@hotmail.com>
2022-06-02 02:33:15 -0500

    Mss

--- cliente.py
+++ cliente.py
@@ -6,14 +6,17 @@ SERVER_ADDRESS = ('127.0.1.1', 1234)
 HEADER_LENGTH = 10
 FORMAT = 'utf-8'
 DISCONNECT_COMMAND = '/disconnect'
+MSS = []
+LOAD = False
 
-def send_message(message):
+
+def send_message(message, client_socket):
     message_length = f"{len(message):<{HEADER_LENGTH}}"
     client_socket.send(bytes(message_length, FORMAT))
     client_socket.send(bytes(message, FORMAT))
 
 
-def receive_message():
+def receive_message(client_socket):
     while True:
         message_length = client_socket.recv(HEADER_LENGTH).decode(FORMAT)
         if message_length:
@@ -21,37 +24,43 @@ def receive_message():
             return message
 
 
-def receive_messages_in_real_time():
+def receive_messages_in_real_time(client_socket):
     while True:
-        message = receive_message()
+        message = receive_message(client_socket)
         print(message)
 
 
-# Connecting to Server
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(SERVER_ADDRESS)
+def runClient():
+    # Connecting to Server
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(SERVER_ADDRESS)
 
-# Username Prompt
-username = input("Enter your username:\n")
-send_message(username)
+    # Username Prompt
+    username = input("Enter your username:\n")
+    send_message(username, client_socket)
 
-# Server Message for successful connection, and previous messages in server
-print(receive_message())
-number_of_messages = receive_message()
-for _ in range(int(number_of_messages)):
-    print(receive_message())
+    # Server Message for successful connection, and previous messages in server
+    print(receive_message(client_socket))
+    number_of_messages = receive_message(client_socket)
+    for _ in range(int(number_of_messages)):
+        mss = receive_message(client_socket)
+        print(mss)
+        MSS.append(mss)
+    global LOAD
+    LOAD = True
 
-# thread for receiving messages
-receiving_thread = threading.Thread(target=receive_messages_in_real_time)
-receiving_thread.daemon = True
-receiving_thread.start()
 
-# messaging in the server
-connected = True
-while connected:
-    message = input(f"<Me> ")
-    if message == DISCONNECT_COMMAND:
-        send_message(f"The user {username} has disconnected!")
-        connected = False
-    else:
-        send_message(message)
+    # thread for receiving messages
+    receiving_thread = threading.Thread(target=receive_messages_in_real_time, args=[client_socket])
+    receiving_thread.daemon = True
+    receiving_thread.start()
+
+    # messaging in the server
+    connected = True
+    while connected:
+        message = input(f"<Me> ")
+        if message == DISCONNECT_COMMAND:
+            send_message(f"The user {username} has disconnected!", client_socket)
+            connected = False
+        else:
+            send_message(message, client_socket)
--- main.py
+++ main.py
@@ -1,46 +1,77 @@
+import threading
 from tkinter import *
 from tkinter import ttk
 import tkinter
-from traceback import print_tb
+from tkinter import filedialog
 
+import cliente
+
+#def loadWindow():
 root = Tk()
 
 frm = ttk.Frame(root, padding=10)
 frm.grid(padx=5, pady=10)
 
 #Vars
-texto = ""
+url_file = StringVar()
+url_file.set("Aun no hay archivo")
+loadOnce = True
 #####
 
 #Funcs
 def setTextBox(widget, text):
+    widget.configure(state="normal")
     widget.insert(tkinter.END, text)
-    print(text)
+    widget.configure(state="disable")
     widget.update()
+
+def openFile(url_file):
+    url = filedialog.askopenfilename(initialdir="/", title="Select file",
+                                filetypes=(("jpeg files", "*.jpg"), ("all files", "*.*")))
+    url_file.set(url)
+
+
+def loadMss(widget):
+    while(True):
+        if(cliente.LOAD):
+            widget.configure(state="normal")
+            for text in cliente.MSS:
+                widget.insert(tkinter.END, (text+"\n"))
+            widget.configure(state="disable")
+            widget.update()
+            cliente.LOAD = False
 
 ######
 
 ttk.Label(frm, text="Chat...", justify="left").grid(column=0, row=0, sticky="w")
 
 sb = ttk.Scrollbar(frm)
-cuadro_texto = tkinter.Text(frm, height=20, width=50, pady=10, padx=10, yscrollcommand=Scrollbar(root).set)
+cuadro_texto = tkinter.Text(frm, state="disable", height=20, width=50, pady=10, padx=10, yscrollcommand=Scrollbar(root).set)
 cuadro_texto.grid(column=0, row=1, sticky="w", pady=10)
 #cuadro_texto.update()
 # Obtener info de la pantalla: cuadro_texto.get("1.0","end-1c")
 sb.grid(column=1, row=1, sticky="wns") 
 sb.config(command=cuadro_texto.yview)
 
-chat = ttk.Entry(frm, width=50, textvariable= texto)
+chat = ttk.Entry(frm, width=55)
 chat.grid(column=0, row=3, sticky="sw")
 
 
 ttk.Button(frm, text="Eviar mensaje", command=lambda: setTextBox(cuadro_texto, (chat.get()+"\n"))
             ).grid(column=1, row=3, sticky="sw", padx=5)
- 
+
 ttk.Separator(frm).grid(column=4, row=0)
-ttk.Label(frm, text="Aqui va un archivo", justify="left").grid(column=5, row=0, sticky="s")
-ttk.Button(frm, text="Subir archivo").grid(column=5, row=1, sticky="n")
+labelURL = ttk.Label(frm, textvariable=url_file, justify="left")
+labelURL.grid(column=5, row=0, sticky="s")
+botonFile = ttk.Button(frm, text="Subir archivo", command=lambda:openFile(url_file))
+botonFile.grid(column=5, row=1, sticky="n")
 
+if(loadOnce):
+    receiving_thread = threading.Thread(target=cliente.runClient)
+    receiving_thread.start()
 
-
+    thread_box = threading.Thread(target=loadMss, args=[cuadro_texto])
+    thread_box.start()
+    loadOnce = False 
+    
 root.mainloop()
